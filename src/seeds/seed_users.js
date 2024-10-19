4480720fce638d813618c3e22b6ad82edee0a30a
bxstiuwu <130089219+bxstiuwu@users.noreply.github.com>
2024-10-18 23:34:45 -0300

    Update seed_users.js

--- src/seeds/seed_users.js
+++ src/seeds/seed_users.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
       .then(function () {
         // Inserts seed entries
         return knex('users').insert([
-          { username: 'admin', password: '123', role: 'admin' },
+          { username: 'felipe', password: '123', role: 'admin' },
           { username: 'master', password: '123', role: 'maestro' },
         ]);
       });
