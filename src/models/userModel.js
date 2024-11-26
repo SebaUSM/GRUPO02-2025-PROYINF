1b20ae3db37c2289a8a74e1abbc5191175b8db3a
SebaUSM <164564727+SebaUSM@users.noreply.github.com>
2024-11-25 21:08:27 -0300

    Delete src directory

--- src/models/userModel.js
+++
@@ -1,9 +0,0 @@
-const db = require('../config/db'); // Asegúrate de que la ruta sea correcta
-
-const getUserByUsername = async (username) => {
-    const result = await db('users').where({ username }).first(); // Usa la sintaxis de Knex
-    return result;
-};
-
-
-module.exports = { getUserByUsername};
